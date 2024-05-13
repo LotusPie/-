08b58dbaed4a5615668eaab9c9c62de9baabd8c8
Somnus <po0919304578@gmail.com>
2024-05-13 14:16:36 +0800

    first try

---
+++ html/kaguya-sama/script-kaguya-sama3.js
@@ -0,0 +1,24 @@
+/*function max_num (num1,num2,num3,){
+    if(max_num){
+    num1>num2&&num1>num3
+    document.write(num1)
+    return unm1;
+}
+    else if(max_num){
+    num2>num3&&num2>num1
+    document.write(num2)
+    return num2;
+}
+    else{
+    num3>num1&&num3>num2
+    document.write(num3)
+    return unm3;
+}
+}
+document.write(max_num(7,8,9));*/
+var me ={
+    name:Jack,
+    age:16,
+    is_male:true
+};
+document.write(me)
